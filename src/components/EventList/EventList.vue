5d6aeae13df76ab3bac1e20f93a154d3c5be6348
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-02-10 22:14:46 +0900

    탭 수정

--- src/components/EventList/EventList.vue
+++ src/components/EventList/EventList.vue
@@ -2,50 +2,83 @@
 <template>
   <div>
     <ul class="eventTab">
-        <li v-for="(item, index) in eventTab" :key="index" @click="tabBt(item)">
-            {{ item }}
-        </li>
+      <li
+        v-for="(item, index) in eventTab"
+        :key="index"
+        @click="tabBt(item, index)"
+        ref="tab"
+      >
+        {{ item }}
+      </li>
     </ul>
-    <EventTable :selected-tab="selectedTab" :default-selecttab="defaultSelectTab" :is-active="isActive" />
+    <EventTable
+      v-if="selectedTab.length > 0"
+      :selected-tab="selectedTab"
+      :is-active="isActive"
+    />
   </div>
 </template>
 
 <script>
-import EventTable from '@/components/EventList/EventTable.vue'
-import data from '@/api/data.json'
+import EventTable from '@/components/EventList/EventTable.vue';
+import data from '@/api/data.json';
 
 export default {
-    components:{
-        EventTable
-    },
-    data(){
-        return {
-            eventList: data,
-            eventTab: Object.keys(data),
-            selectedTab: [],
-            isActive: false,
-            defaultSelectTab: []
+  components: {
+    EventTable,
+  },
+  data() {
+    return {
+      eventList: data,
+      eventTab: Object.keys(data),
+      selectedTab: [],
+      isActive: false,
+    };
+  },
+  created() {
+    var currentTime = new Date();
+    var thisYear = currentTime.getFullYear();
+    data[thisYear].forEach((v, i) => {
+      return this.selectedTab.push(data[2023][i]);
+    });
+    this.isActive = true;
+  },
+  mounted() {
+    const lastTab = this.$refs.tab;
+    lastTab.forEach((v, i) => {
+      if (lastTab.length - 1 === i) {
+        lastTab[i].classList.add('on');
+      }
+    });
+  },
+  methods: {
+    tabBt(eventYear, tabIndex) {
+      this.selectedTab = [];
+      this.eventList[eventYear].map((v, i) => {
+        this.selectedTab.push(this.eventList[eventYear][i]);
+      });
+      const tab = this.$refs.tab;
+      tab.map((v, i) => {
+        if (i === tabIndex) {
+          tab[i].classList.add('on');
+        } else {
+          tab[i].classList.remove('on');
         }
+      });
     },
-    created(){
-        //console.log(this.eventList[2022])
-        //console.log(Object.keys(data))
-        this.defaultSelectTab.push(data[2023][0])
-    },    
-    methods:{
-        tabBt(eventYear){
-            this.selectedTab = []
-            this.eventList[eventYear].map((v,i) => {
-                this.selectedTab.push(this.eventList[eventYear][i])
-            })  
-        }
-    }
+  },
 };
 </script>
 
 <style>
 .eventTab li {
-    display: inline-block;
-    margin:0 15px;
+  display: inline-block;
+  margin: 0 15px;
+}
+.eventTab li:hover {
+  cursor: pointer;
+}
+.eventTab li.on {
+  color: red;
 }
 </style>
--- src/components/EventList/EventTable.vue
+++ src/components/EventList/EventTable.vue
@@ -1,64 +1,61 @@
 <template>
   <div>
     <table v-if="selectedTab[0] !== ''">
-        <colgroup>
-            <col width="20%">
-            <col width="20%">
-            <col width="5%">
-            <col width="15%">
-            <col width="10%">
-            <col width="5%">
-            <col width="5%">
-            <col width="5%">
-            <!-- <col width="10%">
+      <colgroup>
+        <col width="20%" />
+        <col width="20%" />
+        <col width="5%" />
+        <col width="15%" />
+        <col width="10%" />
+        <col width="5%" />
+        <col width="5%" />
+        <col width="5%" />
+        <!-- <col width="10%">
             <col width="10%"> -->
-        </colgroup>
-        <thead>
-            <tr>
-                <th>이름</th>
-                <th>내용</th>
-                <th>이벤트코드</th>
-                <th>소스위치</th>
-                <th>기능</th>
-                <th>미리보기</th>
-                <th>바로가기</th>
-                <th>일감</th>
-                <!-- <th>sp</th>
+      </colgroup>
+      <thead>
+        <tr>
+          <th>이름</th>
+          <th>내용</th>
+          <th>이벤트코드</th>
+          <th>소스위치</th>
+          <th>기능</th>
+          <th>미리보기</th>
+          <th>바로가기</th>
+          <th>일감</th>
+          <!-- <th>sp</th>
                 <th>비고</th> -->
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(item, index) in selectedTab" :key="index">
-                <td>
-                    {{item.name}}
-                </td>
-                <td>
-                    {{item.content}}
-                </td>
-                <td>
-                    {{item.code}}
-                </td>
-                <td>
-                    {{item.place}}
-                </td>
-                <td>
-                    {{item.function}}
-                </td>
-                <td>
-                    <div                         
-                        class="previewWrap"
-                        @click="doMouseOver(item.pageUrl)" 
-                    >
-                        <a href="javascript:;">미리보기</a>
-                    </div>
-                </td>
-                <td>
-                    <a :href="item.pageUrl" target="_blank">index.html</a>
-                </td>
-                <td>
-                    <a :href="item.workUrl" target="_blank">redmine</a>
-                </td>
-                <!--<td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in selectedTab" :key="index">
+          <td>
+            {{ item.name }}
+          </td>
+          <td>
+            {{ item.content }}
+          </td>
+          <td>
+            {{ item.code }}
+          </td>
+          <td>
+            {{ item.place }}
+          </td>
+          <td>
+            {{ item.function }}
+          </td>
+          <td>
+            <div class="previewWrap" @mouseover="doMouseOver(item.pageUrl)">
+              <a href="javascript:;">미리보기</a>
+            </div>
+          </td>
+          <td>
+            <a :href="item.pageUrl" target="_blank">index.html</a>
+          </td>
+          <td>
+            <a :href="item.workUrl" target="_blank">redmine</a>
+          </td>
+          <!--<td>
                     <ul>
                         <li v-for="(sp, i) in item.tableSp.newSp" :key="i">
                             {{sp}}
@@ -66,123 +63,130 @@
                     </ul>
                 </td>      
                 <td>{{item.etc}}</td>           -->
-            </tr>
-        </tbody>
-    </table>    
-    <table v-else>
-        <thead>
-            <tr>
-                <th>1depth</th>
-                <th>2depth</th>
-                <th>Link</th>
-                <th>작업일</th>
-                <th>업데이트일</th>
-                <th>작업일</th>
-                <th>업데이트일</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(item, index) in defaultSelectTab" :key="index">
-                <td>
-                    {{item.name}}
-                </td>
-                <td>
-                    {{item.content}}
-                </td>
-                <td>
-                    {{item.place}}
-                </td>
-                <td>
-                    {{item.function}}
-                </td>
-                <td>
-                    {{item.pageUrl}}
-                </td>
-                <td>
-                    {{item.workUrl}}
-                </td>
-                <td>
-                    <ul>
-                        <li v-for="(sp, i) in item.tableSp.newSp" :key="i">
-                            {{sp}}
-                        </li>
-                    </ul>
-                </td>                
-            </tr>
-        </tbody>
+        </tr>
+      </tbody>
     </table>
-    <div class="previewPop">
-        <button @click="closePreviewPop">X</button>
-        <Iframe :sel-page-url="selPageUrl" />
+    <table v-else>
+      <thead>
+        <tr>
+          <th>1depth</th>
+          <th>2depth</th>
+          <th>Link</th>
+          <th>작업일</th>
+          <th>업데이트일</th>
+          <th>작업일</th>
+          <th>업데이트일</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in defaultSelectTab" :key="index">
+          <td>
+            {{ item.name }}
+          </td>
+          <td>
+            {{ item.content }}
+          </td>
+          <td>
+            {{ item.place }}
+          </td>
+          <td>
+            {{ item.function }}
+          </td>
+          <td>
+            {{ item.pageUrl }}
+          </td>
+          <td>
+            {{ item.workUrl }}
+          </td>
+          <td>
+            <ul>
+              <li v-for="(sp, i) in item.tableSp.newSp" :key="i">
+                {{ sp }}
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="previewPop" ref="popup">
+      <button @click="closePreviewPop">X</button>
+      <Iframe :sel-page-url="selPageUrl" />
     </div>
   </div>
 </template>
 
 <script>
-import Iframe from '@/components/iframe/Iframe'
+import Iframe from '@/components/iframe/Iframe';
 
 export default {
-    props:{
-        selectedTab: Array,
-        defaultSelectTab: Array
+  props: {
+    selectedTab: Array,
+  },
+  data() {
+    return {
+      preview: this.selectedTab,
+      selpreview: '',
+      selPageUrl: '',
+      showSearch: false,
+    };
+  },
+  components: {
+    Iframe,
+  },
+  //   created() {
+  //     window.addEventListener('click', this.closepop);
+  //   },
+  //   destroyed() {
+  //     window.removeEventListener('click', this.closepop);
+  //   },
+  methods: {
+    doMouseOver(e) {
+      // const target = document.querySelector(`.area${e}`)
+      // target.classList.add('selectedFrame')
+      const target = document.querySelector('.previewPop');
+      this.selPageUrl = e;
+      target.style.display = 'block';
+      //const eachFrame = e.target;
+      //console.log(eachFrame)
+      //eachFrame.classList.add('selectedFrame')
     },
-    data(){
-        return {
-            preview:this.selectedTab,
-            selpreview:'',
-            selPageUrl:''
-        }
+    closePreviewPop(e) {
+      this.showSearch = true;
+      const target = e.target.parentElement;
+      target.style.display = 'none';
     },
-    components:{
-        Iframe
+    closepop() {
+      //   const target = document.querySelector('.previewPop');
+      //   console.log(target);
+      //   target.style.display = 'none';
     },
-    mounted(){
-        // console.log(this.defaultSelectTab)
-        console.log(this.preview)     
-    },
-    methods:{
-        doMouseOver(e){
-            console.log(e)
-            // const target = document.querySelector(`.area${e}`)
-            // target.classList.add('selectedFrame')
-            const target = document.querySelector('.previewPop')
-            this.selPageUrl = e
-            target.style.display = 'block'
-            //const eachFrame = e.target;
-            //console.log(eachFrame)
-            //eachFrame.classList.add('selectedFrame')
-        },
-        closePreviewPop(e){
-            const target = e.target.parentElement
-            target.style.display = 'none'
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 .area {
 }
 .area > iframe {
-    width: 300px; 
-    height: 200px; 
-    resize: both; 
+  width: 300px;
+  height: 200px;
+  resize: both;
 }
 .selectedFrame {
-    position: absolute;
-    left: 150px;
-    width: 1000px;
-    height: 1000px;
+  position: absolute;
+  left: 150px;
+  width: 1000px;
+  height: 1000px;
 }
 .previewPop {
-    position: absolute;
-    left: 150px;
-    width: 1100px;
-    height: 50vh;
-    display: none;    
-    text-align: right;
+  position: absolute;
+  left: 150px;
+  width: 1100px;
+  height: 50vh;
+  display: none;
+  text-align: right;
 }
 .previewWrap {
-    z-index: 9999;
+  z-index: 9999;
 }
 </style>

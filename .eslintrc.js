19b24615f5135a6d8f1e20ce0f6f84297cec802c
황승현 <ghkdsigm3@gmail.com>
2023-02-14 16:21:33 +0900

    netlify update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,25 @@
 // .eslintrc.js
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": "eslint:recommended",
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"]
-    },    
-    "plugins": [
-        "vue"
-    ],
-}
+    root: true,
+    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    parserOptions: {
+      parser: "babel-eslint",
+    },
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": "off",
+      // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+      // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+      "prettier/prettier": ['error', {
+        singleQuote: false,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      }]
+    },
+  };
